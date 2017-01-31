599eeb7f01f0abcfce5a2690f08f454bd2c74ecc
ElKartofel <michal96majda@gmail.com>
2017-01-31 22:20:12 +0100

    Update Form1.Designer.cs

--- przeglądarka/przeglądarka/Form1.Designer.cs
+++ przeglądarka/przeglądarka/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace przeglądarka
+namespace przeglądarka
 {
     partial class Form1
     {
@@ -35,6 +35,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -47,7 +48,6 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button11 = new System.Windows.Forms.Button();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -56,9 +56,10 @@
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(378, 49);
+            this.textBox1.Location = new System.Drawing.Point(284, 40);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(787, 22);
+            this.textBox1.Size = new System.Drawing.Size(591, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
@@ -70,7 +71,8 @@
             this.plikToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1262, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(946, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -79,13 +81,13 @@
             this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.wyjdźToolStripMenuItem});
             this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
-            this.plikToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
             this.plikToolStripMenuItem.Text = "Plik";
             // 
             // wyjdźToolStripMenuItem
             // 
             this.wyjdźToolStripMenuItem.Name = "wyjdźToolStripMenuItem";
-            this.wyjdźToolStripMenuItem.Size = new System.Drawing.Size(125, 26);
+            this.wyjdźToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
             this.wyjdźToolStripMenuItem.Text = "Wyjdź";
             this.wyjdźToolStripMenuItem.Click += new System.EventHandler(this.wyjdźToolStripMenuItem_Click);
             // 
@@ -93,9 +95,10 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button1.Location = new System.Drawing.Point(1171, 42);
+            this.button1.Location = new System.Drawing.Point(878, 34);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 37);
+            this.button1.Size = new System.Drawing.Size(47, 30);
             this.button1.TabIndex = 4;
             this.button1.Text = ">>";
             this.button1.UseVisualStyleBackColor = false;
@@ -108,23 +111,31 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.toolStripProgressBar1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 649);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 525);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1262, 24);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(946, 22);
             this.statusStrip1.TabIndex = 5;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 20);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.CausesValidation = false;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(75, 16);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button2.Location = new System.Drawing.Point(290, 42);
+            this.button2.Location = new System.Drawing.Point(218, 34);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(37, 37);
+            this.button2.Size = new System.Drawing.Size(28, 30);
             this.button2.TabIndex = 6;
             this.button2.Text = "<<";
             this.button2.UseVisualStyleBackColor = false;
@@ -133,9 +144,10 @@
             // button4
             // 
             this.button4.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button4.Location = new System.Drawing.Point(182, 42);
+            this.button4.Location = new System.Drawing.Point(136, 34);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 37);
+            this.button4.Size = new System.Drawing.Size(56, 30);
             this.button4.TabIndex = 9;
             this.button4.Text = "Odśwież";
             this.button4.UseVisualStyleBackColor = false;
@@ -144,9 +156,10 @@
             // button5
             // 
             this.button5.BackColor = System.Drawing.Color.Red;
-            this.button5.Location = new System.Drawing.Point(104, 41);
+            this.button5.Location = new System.Drawing.Point(78, 33);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(72, 38);
+            this.button5.Size = new System.Drawing.Size(54, 31);
             this.button5.TabIndex = 10;
             this.button5.Text = "Stop";
             this.button5.UseVisualStyleBackColor = false;
@@ -155,9 +168,10 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button3.Location = new System.Drawing.Point(333, 42);
+            this.button3.Location = new System.Drawing.Point(250, 34);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(39, 37);
+            this.button3.Size = new System.Drawing.Size(29, 30);
             this.button3.TabIndex = 7;
             this.button3.Text = ">>";
             this.button3.UseVisualStyleBackColor = false;
@@ -170,9 +184,10 @@
             this.button6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.button6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button6.Location = new System.Drawing.Point(196, 233);
+            this.button6.Location = new System.Drawing.Point(147, 189);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(148, 125);
+            this.button6.Size = new System.Drawing.Size(111, 102);
             this.button6.TabIndex = 14;
             this.button6.Text = "Google";
             this.button6.UseVisualStyleBackColor = false;
@@ -184,9 +199,10 @@
             this.button7.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button7.BackColor = System.Drawing.Color.Red;
             this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button7.Location = new System.Drawing.Point(401, 233);
+            this.button7.Location = new System.Drawing.Point(301, 189);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(156, 125);
+            this.button7.Size = new System.Drawing.Size(117, 102);
             this.button7.TabIndex = 16;
             this.button7.Text = "WEMiF";
             this.button7.UseVisualStyleBackColor = false;
@@ -198,9 +214,10 @@
             this.button8.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button8.BackColor = System.Drawing.Color.Yellow;
             this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button8.Location = new System.Drawing.Point(617, 233);
+            this.button8.Location = new System.Drawing.Point(463, 189);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(151, 125);
+            this.button8.Size = new System.Drawing.Size(113, 102);
             this.button8.TabIndex = 17;
             this.button8.Text = "Onet";
             this.button8.UseVisualStyleBackColor = false;
@@ -212,9 +229,10 @@
             this.button9.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button9.Location = new System.Drawing.Point(826, 233);
+            this.button9.Location = new System.Drawing.Point(620, 189);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(167, 125);
+            this.button9.Size = new System.Drawing.Size(125, 102);
             this.button9.TabIndex = 18;
             this.button9.Text = "Rozkład jazdy \r\nMPK";
             this.button9.UseVisualStyleBackColor = false;
@@ -223,9 +241,10 @@
             // button10
             // 
             this.button10.BackColor = System.Drawing.Color.Chartreuse;
-            this.button10.Location = new System.Drawing.Point(23, 41);
+            this.button10.Location = new System.Drawing.Point(17, 33);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(75, 38);
+            this.button10.Size = new System.Drawing.Size(56, 31);
             this.button10.TabIndex = 19;
             this.button10.Text = "Home";
             this.button10.UseVisualStyleBackColor = false;
@@ -234,19 +253,22 @@
             // textBox2
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox2.Location = new System.Drawing.Point(104, 196);
+            this.textBox2.Location = new System.Drawing.Point(78, 159);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(926, 22);
+            this.textBox2.Size = new System.Drawing.Size(696, 20);
             this.textBox2.TabIndex = 21;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // button11
             // 
             this.button11.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button11.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button11.Location = new System.Drawing.Point(1036, 187);
+            this.button11.Location = new System.Drawing.Point(777, 152);
+            this.button11.Margin = new System.Windows.Forms.Padding(2);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(178, 40);
+            this.button11.Size = new System.Drawing.Size(134, 32);
             this.button11.TabIndex = 22;
             this.button11.Text = "Szukaj w Google";
             this.button11.UseVisualStyleBackColor = false;
@@ -257,26 +279,24 @@
             this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.webBrowser1.Location = new System.Drawing.Point(12, 85);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Location = new System.Drawing.Point(9, 69);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(15, 16);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.ScriptErrorsSuppressed = true;
-            this.webBrowser1.Size = new System.Drawing.Size(1238, 560);
+            this.webBrowser1.Size = new System.Drawing.Size(928, 455);
             this.webBrowser1.TabIndex = 23;
             this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
             this.webBrowser1.Visible = false;
-            // 
-            // toolStripProgressBar1
-            // 
-            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 18);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser1_ProgressChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.ClientSize = new System.Drawing.Size(946, 547);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.button11);
             this.Controls.Add(this.textBox2);
@@ -294,6 +314,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Przeglądarka";
             this.menuStrip1.ResumeLayout(false);
@@ -328,4 +349,3 @@
         private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
     }
 }
-
